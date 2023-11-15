3d09f55996b822a24fe49b094f154ee43374fe59
imchaewon <imchaewon26@gmail.com>
2023-11-15 18:06:29 +0900

    spring 등

--- test-project/src/main/java/com/example/java_/jsonParsing/pojo/jackson/jsonSerialize/JsonTypeInfo_JsonSubTypes/t2/Rectangle.java
+++ test-project/src/main/java/com/example/java_/jsonParsing/pojo/jackson/jsonSerialize/JsonTypeInfo_JsonSubTypes/t2/Rectangle.java
@@ -1,10 +1,12 @@
 package com.example.java_.jsonParsing.pojo.jackson.jsonSerialize.JsonTypeInfo_JsonSubTypes.t2;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class Rectangle extends Shape {
 	private int width;
 	private int height;
