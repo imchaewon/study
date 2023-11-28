88679ed77eb90d24806ba3c22db0be3bcc693ac1
imchaewon <imchaewon26@gmail.com>
2023-11-29 01:26:05 +0900

    no message

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -103,6 +103,17 @@ public class OrderRepository {
 				.getResultList();
 	}
 
+	public List<Order> findAllWithItem() {
+		return em.createQuery("select distinct o from Order o" +
+				" join fetch o.member m" +
+				" join fetch o.delivery d" +
+				" join fetch o.orderItems oi" +
+				" join fetch oi.item i", Order.class)
+				.setFirstResult(1)
+				.setMaxResults(100)
+				.getResultList();
+	}
+
 //	public List<Order> findAll(OrderSearch orderSearch) {
 //		QOrder order = QOrder.order;
 //		QMember member = QMember.member;
