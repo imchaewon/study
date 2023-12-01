7dfc254e5ee41f0ff5043a55a066fa5a135c0cbd
imchaewon <imchaewon26@gmail.com>
2023-12-02 01:17:58 +0900

    no message

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,6 +1,11 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
 import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -90,9 +95,33 @@ public class OrderRepository {
 		return query.getResultList();
 	}
 
-//	public List<Order> findAll(OrderSearch orderSearch) {
-//
-//	}
+	public List<Order> findAll(OrderSearch orderSearch) {
+		QOrder order = QOrder.order;
+		QMember member = QMember.member;
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		return query
+				.select(order)
+				.from(order)
+				.join(order.member, member)
+				.where(order.status.eq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+				.limit(1000)
+				.fetch();
+	}
+
+	private static BooleanExpression nameLike(String memberName) {
+		if (!StringUtils.hasText(memberName)) {
+			return null;
+		}
+		return QMember.member.name.like(memberName);
+	}
+
+	private BooleanExpression statusEq(OrderStatus statusCond) {
+		if (statusCond == null) {
+			return null;
+		}
+		return QOrder.order.status.eq(statusCond);
+	}
 
 	public List<Order> findAllWithMemberDelivery() {
 		return em.createQuery(
