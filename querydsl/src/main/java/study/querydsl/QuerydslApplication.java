eef8a23d87fcf933e8dfdfb8edc6000e6edf5163
imchaewon <imchaewon26@gmail.com>
2023-12-05 00:41:03 +0900

    QueryDSL

--- querydsl/src/main/java/study/querydsl/QuerydslApplication.java
+++ querydsl/src/main/java/study/querydsl/QuerydslApplication.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class QuerydslApplication {
--- querydsl/src/main/java/study/querydsl/entity/Hello.java
+++ querydsl/src/main/java/study/querydsl/entity/Hello.java
@@ -2,6 +2,7 @@ package study.querydsl.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,6 +10,6 @@ import lombok.Setter;
 @Entity
 @Getter @Setter
 public class Hello {
-	@Id @GeneratedValue
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 }
