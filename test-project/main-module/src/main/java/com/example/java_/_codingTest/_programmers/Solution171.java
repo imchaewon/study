40a0c635ad1fee964af017a8c40c2a0de4d28840
imchaewon <imchaewon26@gmail.com>
2025-01-02 15:17:51 +0900

    코테

--- test-project/main-module/src/main/java/com/example/java_/_codingTest/_programmers/Solution171.java
+++ test-project/main-module/src/main/java/com/example/java_/_codingTest/_programmers/Solution171.java
@@ -10,6 +10,9 @@ package com.example.java_._codingTest._programmers;
 // 즉, 2개의 햄버거를 포장하게 됩니다.
 //
 //상수에게 전해지는 재료의 정보를 나타내는 정수 배열 ingredient가 주어졌을 때, 상수가 포장하는 햄버거의 개수를 return 하도록 solution 함수를 완성하시오.
+// 1:빵 2:야채 3:고기
+
+import java.util.Stack;
 
 public class Solution171 {
 	public static void main(String[] args) {
@@ -20,7 +23,40 @@ public class Solution171 {
 
 	public int solution(int[] ingredient) {
 		int answer = 0;
-		
+		Stack<Integer> stack = new Stack<>();
+		for (int i : ingredient) {
+			stack.push(i);
+			int size = stack.size();
+			if(size >= 4){
+				if(stack.get(size - 4) == 1 &&
+					stack.get(size - 3) == 2 &&
+					stack.get(size - 2) == 3 &&
+					stack.get(size - 1) == 1){
+					stack.pop();
+					stack.pop();
+					stack.pop();
+					stack.pop();
+					answer++;
+				}
+			}
+		}
+		return answer;
+	}
+
+	public int solution2(int[] ingredient) {
+		int[] stack = new int[ingredient.length];
+		int sp = 0;
+		int answer = 0;
+		for (int i : ingredient) {
+			stack[sp++] = i;
+			if (sp >= 4 && stack[sp - 1] == 1
+				&& stack[sp - 2] == 3
+				&& stack[sp - 3] == 2
+				&& stack[sp - 4] == 1) {
+				sp -= 4;
+				answer++;
+			}
+		}
 		return answer;
 	}
 }
